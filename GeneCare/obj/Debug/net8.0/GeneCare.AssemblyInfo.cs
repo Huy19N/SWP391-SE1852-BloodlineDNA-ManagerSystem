3c9eae8053d431d070691cd09673131dfe2b87fe
Tad-Min <183442531+Tad-Min@users.noreply.github.com>
2025-06-03 11:12:23 +0700

    fix

--- GeneCare/obj/Debug/net8.0/GeneCare.AssemblyInfo.cs
+++ GeneCare/obj/Debug/net8.0/GeneCare.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GeneCare")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+804cab97102afd70555dbb3f4f88a8f836982f4c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d8ea53aec291ad535673c9c46a93b86f7fbd87c")]
 [assembly: System.Reflection.AssemblyProductAttribute("GeneCare")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GeneCare")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
