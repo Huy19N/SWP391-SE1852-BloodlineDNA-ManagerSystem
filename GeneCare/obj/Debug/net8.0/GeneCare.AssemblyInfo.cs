8b79cf998e59809201f91e6f54d56d030203a732
anhbathung <hieuthuan833@gmail.com>
2025-06-03 10:27:47 +0700

    .

--- GeneCare/obj/Debug/net8.0/GeneCare.AssemblyInfo.cs
+++ GeneCare/obj/Debug/net8.0/GeneCare.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GeneCare")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b043b727102e1164b628108bb60d98fb6d11fc36")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e077d8f99940aef643a77490ad1965ad45b5ff86")]
 [assembly: System.Reflection.AssemblyProductAttribute("GeneCare")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GeneCare")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
