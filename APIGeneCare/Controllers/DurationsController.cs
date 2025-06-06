c1b619c401cb94b701037c1fd3b2101aac046e7c
Tad-Min <183442531+Tad-Min@users.noreply.github.com>
2025-06-06 15:39:50 +0700

    CRUD

---
+++ APIGeneCare/Controllers/DurationsController.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using APIGeneCare.Data;
+
+namespace APIGeneCare.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DurationsController : ControllerBase
+    {
+        private readonly GeneCareContext _context;
+
+        public DurationsController(GeneCareContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Durations
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Duration>>> GetDurations()
+        {
+            return await _context.Durations.ToListAsync();
+        }
+
+        // GET: api/Durations/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Duration>> GetDuration(int id)
+        {
+            var duration = await _context.Durations.FindAsync(id);
+
+            if (duration == null)
+            {
+                return NotFound();
+            }
+
+            return duration;
+        }
+
+        // PUT: api/Durations/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutDuration(int id, Duration duration)
+        {
+            if (id != duration.DurationId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(duration).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!DurationExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Durations
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Duration>> PostDuration(Duration duration)
+        {
+            _context.Durations.Add(duration);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (DurationExists(duration.DurationId))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetDuration", new { id = duration.DurationId }, duration);
+        }
+
+        // DELETE: api/Durations/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteDuration(int id)
+        {
+            var duration = await _context.Durations.FindAsync(id);
+            if (duration == null)
+            {
+                return NotFound();
+            }
+
+            _context.Durations.Remove(duration);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool DurationExists(int id)
+        {
+            return _context.Durations.Any(e => e.DurationId == id);
+        }
+    }
+}
---
+++ APIGeneCare/Controllers/SamplesController.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using APIGeneCare.Data;
+
+namespace APIGeneCare.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SamplesController : ControllerBase
+    {
+        private readonly GeneCareContext _context;
+
+        public SamplesController(GeneCareContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Samples
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Sample>>> GetSamples()
+        {
+            return await _context.Samples.ToListAsync();
+        }
+
+        // GET: api/Samples/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Sample>> GetSample(int id)
+        {
+            var sample = await _context.Samples.FindAsync(id);
+
+            if (sample == null)
+            {
+                return NotFound();
+            }
+
+            return sample;
+        }
+
+        // PUT: api/Samples/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutSample(int id, Sample sample)
+        {
+            if (id != sample.SampleId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(sample).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!SampleExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Samples
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Sample>> PostSample(Sample sample)
+        {
+            _context.Samples.Add(sample);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (SampleExists(sample.SampleId))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetSample", new { id = sample.SampleId }, sample);
+        }
+
+        // DELETE: api/Samples/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteSample(int id)
+        {
+            var sample = await _context.Samples.FindAsync(id);
+            if (sample == null)
+            {
+                return NotFound();
+            }
+
+            _context.Samples.Remove(sample);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool SampleExists(int id)
+        {
+            return _context.Samples.Any(e => e.SampleId == id);
+        }
+    }
+}
