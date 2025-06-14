c56174a178d990298c08e8f8a248ef8d6e0b029e
Kienht7704 <kienht7704@gmail.com>
2025-06-14 17:27:28 +0700

    Dropdown and css

---
+++ FE/GenCare/src/components/InstructionDropdown.jsx
@@ -0,0 +1,14 @@
+
+function InstructionDropdown()  {
+    return(
+        <div className="felx felx-col InstructionDropdown">
+            <ul className="felx felx-col gap-4">
+                <li>Huong da xet nghiem</li>
+                <li>Huong da thanh toan</li>
+                <li>Huong da lay mau</li>
+            </ul>
+        </div>
+    );
+}
+
+export default InstructionDropdown
